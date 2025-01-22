cf0e5284a444b94f50a5ace8467cce7e3cdc390a
Anyeel <angelmillanaragon@hotmail.com>
2025-01-22 12:06:52 +0100

    feat: lista de usuarios aleatoria

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require("express")
 
 const app = express()
 
-const users = [
-    {
-        "username": "admin",
-        "password": "admin",
-        "email": "admin@gmail.com",
-        "status": "active"
-    },
-    {
-        "username": "user1",
-        "password": "user1",
-        "email": "user1@gmail.com",
-        "status": "active"
-    }
-]
+const listaAdejtivos = ["brillante", "curioso", "temerario", "misterioso", "singular", "elegante", "rápido", "sincero", "audaz", "luminoso"]
+const listaNombres = ["Alejandro", "Beatriz", "Carlos", "Diana", "Elena", "Fernando", "Gabriela", "Hugo", "Isabel", "Javier"]
+
+const users = []
+
+for (let i = 0; i < 500; i++){
+    const numeroAleatorio = Math.floor(1000 + Math.random() * 9000)
+    const nombreUsuario = randomFromArray(listaAdejtivos) + randomFromArray(listaNombres) + numeroAleatorio
+    const email = nombreUsuario + "@gmail.com"
+    const contraseña = randomFromArray(listaAdejtivos) + randomFromArray(listaNombres) + numeroAleatorio
+    const estado = randomFromArray(["activo", "inactivo"])
+    const objeto = {nombreUsuario, email, contraseña, estado}
+    users.push(objeto)
+}
 
 app.get("/",(req, res) =>{
     console.log("Hola mundo") //esto se ejecuta en el servidor
@@ -25,6 +25,12 @@ app.get("/",(req, res) =>{
 app.get("/users",(req, res) =>{
     res.json(users)
 })
+
+function randomFromArray(array){
+    const num = Math.random() * array.length
+    const numInt = Math.floor(num)
+    return array[numInt]
+}
 
 app.listen(3000, () => {
     console.log("El servidor está funcionando en el puerto 3000")
